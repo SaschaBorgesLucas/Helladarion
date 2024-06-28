5d66d9b79cb1fe3b9f9c0ac4c16cff0006a5f074
Sascha Borges Lucas <saschaborgeslucas@gmail.com>
2024-06-28 19:48:44 -0400

    configurações do express

--- Helladarion_API/src/Server.ts
+++ Helladarion_API/src/Server.ts
@@ -1,17 +1,15 @@
-const http = require('http');
+import express from "express";
+const porta = 3000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-   res.setHeader('Content-Type', 'text/plain');
- 
-   if (req.url === '/') {
-       res.statusCode = 200;
-       res.end('Home page');
-   } else if (req.url === '/sobre') {
-       res.statusCode = 200;
-       res.end('About page');
-   }
+app.get("/revistas", (req, res) =>{
+    res.send("funfando");
 });
 
-server.listen(3000, () => {
- console.log(`Servidor em execução em http://localhost:3000/`);
+app.get("/capas", (req, res) =>{
+    res.send("funfando");
+});
+
+app.listen(porta, ()=>{
+    console.log(`servidor em execução na porta http://localhost:${porta}/`);
 });
